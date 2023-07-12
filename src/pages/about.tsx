9f6b1d09e28aadcd3808d436d525c879e5f19608
thecmdrunner <thecmdrunner@proton.me>
2023-07-12 14:36:02 +0530

    beta -> alpha

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -43,7 +43,7 @@ const About = () => {
                 </div>
               </address>
               <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">
-                Swiftube v0.1 Beta
+                Swiftube v0.1 Alpha
               </h1>
             </header>
 
